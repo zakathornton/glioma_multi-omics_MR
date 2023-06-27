118821b4522e0daf35ce2e73d9a4181d63a6a6af
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2023-06-27 15:26:48 +0100

    Create data_cleaning_exposure.R

---
+++ GTEx/data_cleaning_exposure.R
@@ -0,0 +1,31 @@
+
+#load modules
+
+library(tidyr)
+
+#load data
+
+data<-read.delim("~/Documents/University/PhD Population Health Sciences/QTL/Exposure/GTEx/PWCoCo/Amygdala/RTEL1/am_RTEL1_SNP.txt", sep=",")
+
+#add sample size
+
+data$N<-175
+
+#filter and order for pwcoco
+
+data<-data[c(1,5,4,10,8,9,7,11)]
+
+#rename columns
+
+colnames(data)[1]<-"SNP"
+colnames(data)[2]<-"A1"
+colnames(data)[3]<-"A2"
+colnames(data)[4]<-"EAF"
+colnames(data)[5]<-"BETA"
+colnames(data)[6]<-"SE"
+colnames(data)[7]<-"P"
+
+#save the new dataset
+
+write.table(data, "~/Documents/University/PhD Population Health Sciences/QTL/Exposure/GTEx/PWCoCo/Amygdala/RTEL1/am_RTEL1_SNP.txt",
+            quote = F, row.names = F)
