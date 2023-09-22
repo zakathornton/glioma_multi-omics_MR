52931dc6837332eb07f3e971273a1b8879d51e35
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2023-09-22 16:31:29 +0100

    Update steiger_filtering.R

--- GTEx/Steiger Filtering/steiger_filtering.R
+++ GTEx/Steiger Filtering/steiger_filtering.R
@@ -1,8 +1,19 @@
 
-#load modules
+#load modules.
 
 library(ieugwasr)
 library(TwoSampleMR)
+library(gwasvcf)
+library(gwasglue)
+library(VariantAnnotation)
+library(dplyr)
+library(genetics.binaRies)
+
+#set the location for plink and bcftools.
+
+gwasvcf::set_plink("/mnt/storage/software/apps/Plink-1.90/plink")
+gwasvcf::set_bcftools()
+ldfile<-"EUR"
 
 #load exposure data
 
@@ -17,18 +28,37 @@ exp_data<-exp_data[exp_data$GENE%in%c("RTEL1"),]
 exp_data<-format_data(exp_data,type="exposure", header=T, snp_col="SNP", beta_col="BETA", se_col="SE", effect_allele_col="A1",
                       other_allele_col="A2", pval_col="P", samplesize_col="N", phenotype_col="GENE", eaf_col="FREQA1")
 
-#clump SNPs
+#load outcome VCF data for all glioma data.
 
-exp_data<-clump_data(exp_data,clump_r2=0.001)
+all_vcf<-VariantAnnotation::readVcf("~/../../work/bp20214/Glioma/all_glioma.vcf")
 
-#load outcome data
+#run a query to find proxy SNPs for SNPs that cannot be found in the outcome data.
 
-out_data<-read.delim("am_SNP_outcome.txt")
+all_data<-gwasvcf::query_gwas(all_vcf, rsid=unique(exp_data), bfile = ldfile, proxies = "yes", tag_r2=0.8, rsidx = "all_index.rsidx")
 
-#format outcome data
+#format VCF for Steiger filtering.
 
-out_data<-format_data(out_data, type="outcome", header=T, snp_col="SNP", beta_col="BETA", se_col="SE",effect_allele_col="A1",
-                      other_allele_col="A2", pval_col="P", phenotype_col="DISEASE", eaf_col="FREQA1")
+all_data<-gwasglue::gwasvcf_to_TwoSampleMR(all_data, type="outcome")
+
+#change the name of outcome column to correct phenotype.
+
+all_data$outcome<-"all"
+
+#repeat the same steps for the gbm and non-gbm data.
+
+gbm_vcf<-VariantAnnotation::readVcf("~/../../work/bp20214/Glioma/gbm_glioma.vcf")
+gbm_data<-gwasvcf::query_gwas(gbm_vcf, rsid=unique(exp_data$SNP), bfile = ldfile, proxies = "yes", tag_r2=0.8, rsidx = "gbm_index.rsidx")
+gbm_data<-gwasglue::gwasvcf_to_TwoSampleMR(gbm_data, type="outcome")
+gbm_data$outcome<-"gbm"
+
+nongbm_vcf<-VariantAnnotation::readVcf("~/../../work/bp20214/Glioma/nongbm_glioma.vcf")
+nongbm_data<-gwasvcf::query_gwas(nongbm_vcf, rsid=unique(exp_data$SNP), bfile = ldfile, proxies = "yes", tag_r2=0.8, rsidx = "nongbm_index.rsidx")
+nongbm_data<-gwasglue::gwasvcf_to_TwoSampleMR(nongbm_data, type="outcome")
+nongbm_data$outcome<-"nongbm"
+
+#combine all the new data into a new dataframe.
+
+out_data<-rbind(all_data,gbm_data,nongbm_data)
 
 #harmonise data
 
