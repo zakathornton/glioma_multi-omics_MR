c33d3809e1a6730283e2bddadeb8d4500d933452
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-25 18:22:22 +0100

    Create pwcoco.sh

---
+++ PWCoCo/pwcoco.sh
@@ -0,0 +1,9 @@
+./pwcoco/build/pwcoco \
+	 --bfile GRCh37_EUR \
+	 --chr 2 \
+	 --maf 0.01 \
+	 --init_h4 100 \
+	 --out_cond \
+	 --sum_stats1 brainQTL_D2HGDH_SNP.txt \
+	 --sum_stats2 brainQTL_D2HGDH_SNP_outcome_all.txt \
+	 --out brainQTL_D2HGDH_all_pwcoco \
